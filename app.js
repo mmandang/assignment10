15094a36bbcec928d538cfdcb811a960f56faa87
mmandang <mmandang@yahoo.com>
2015-05-04 18:17:46 -0700

    deleted config

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 var demoApp = angular.module('demoApp', []);
 
-demoApp.config(function ($routeProvider) {
-	$routeProvider
-		.when('/',{
-				controller: 'SimpleController',
-				templateUrl: 'View.html'
-			})
-		.otherwise({ redirectTo: '/' });
-		
-});
+demoApp.controller('SimpleController', function($scope) {
+    $scope.comments = [
+        'This is the first comment!',
+        'Heres the second one!',
+        'And this is one more.',
+        'Here is another one!'
 
-demoApp.controller('SimpleController', function ($scope) {
- 	$scope.comments = [
- 	     'This is the first comment!',
-         'Heres the second one!',
-         'And this is one more.',
-         'Here is another one!'
+    ];
 
- 	];
-
- 	$scope.addComment = function() {
-		$scope.comments.push($scope.newComment.dt);
-	};
+    $scope.addComment = function() {
+        $scope.comments.push($scope.newComment.dt);
+    };
 
 });
